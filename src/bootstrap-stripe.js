43ef1c9fcc18706d91df375814bb60073a2e9d4e
Robert Rawlins <robert@weboffins.com>
2015-03-28 15:24:38 +0000

    Added basic stripe form submission and callback handling.

--- src/bootstrap-stripe.js
+++ src/bootstrap-stripe.js
@@ -6,8 +6,12 @@
 	// ================================
 
 	var StripeForm = function (element, options) {
+		// Get a reference to the element and set the options.
 	    this.$element  = $(element)
 	    this.options   = $.extend({}, StripeForm.DEFAULTS, options)
+
+	    // Get a reference to the input which will store the card token.
+	    this.$target   = this.$element.find('input[name="stripe_card_token"]')
 
 	    // Get the pubiic stripe key from the data attribute which was passed in.
 	    this.pk 	   = this.$element.data('stripe-form')
@@ -24,9 +28,38 @@
 	StripeForm.DEFAULTS = {}
 
 	StripeForm.prototype.submit = function() {
+		// TODO: Add Disabled/Loading state to the forms button.
+
+		// Submit the form through the stripe API library
+		// binding the async request to a callback method on the form.
+		Stripe.card.createToken(this.$element, $.proxy(this.callback, this));
+
 		// Return false to prevent the form
 		// from actually submitting to the server by itself.
 		return false
+	}
+
+	StripeForm.prototype.callback = function(status, response) {
+		// Check to see if any error ocurred during
+		// the Stripe API request.
+		if (response.error) {
+			// An error has occurred.
+			// Run an alert for the moment.
+			window.alert('something went wrong')
+		} else {
+			// No errors ocurred, the request was a success.
+			// response contains id and card, which contains additional card details
+    		var token = response.id;
+
+    		// Set the hidden card token field on the form with the value
+    		// passed back from the API request.
+    		this.$target.val(token)
+
+    		// Submit the form.
+    		// NOTE: We call submit direct on the DOM 
+    		// element, to prevent us getting into a submit() handler loop.
+    		this.$element.get(0).submit();
+		}
 	}
 
 	// BOOSTRAP STRIPE PLUGIN DEFINITION
