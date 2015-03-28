1d17e63166b50c4cab4921ef88c91b23c19f3360
Robert Rawlins <robert@weboffins.com>
2015-03-28 13:58:55 +0000

    Initial project grunt template with skeleton plugin.

---
+++ Gruntfile.js
@@ -0,0 +1,65 @@
+/*jshint multistr: true */
+module.exports = function(grunt) {
+  'use strict';
+
+  // Project configuration.
+  grunt.initConfig({
+    // Default package configuration.
+    pkg: grunt.file.readJSON('package.json'),
+
+    // Define a banner to added to the compiled assets.
+    banner: '/*!\n' +
+            ' * <%= pkg.name %> v<%= pkg.version %>\n' +
+            ' * (c) Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+            ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n' +
+            ' */\n',
+
+    // Javascript validation.
+    jshint: {
+      options: {
+        jshintrc: 'src/.jshintrc'
+      },
+      all: ['Gruntfile.js', 'src/**/*.js']
+    },
+
+    // Minify Javascript Assets.
+    uglify: {
+      build: {
+        src: 'src/<%= pkg.name %>.js', // Take temporary pre-compiled asset.
+        dest: 'dist/<%= pkg.name %>.min.js' // Plop it in the distribution folder.
+      },
+      options: {
+        banner: '<%= banner %>'
+      }
+    },
+
+    // Watch and instant rebuild.
+    watch: {
+      files: ['index.html', 'src/**/*'],
+      tasks: ['default']
+    },
+
+    // Local demo / development site.
+    connect: {
+      server: {
+        options: {
+          port: 9001,
+          keepalive: true
+        }
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  // Load the plugin that validates the JS markup.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  // Load the plugin that allows us a local HTTP server.
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  // Watcher for rebuilding when files changes.
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+  grunt.registerTask('default', ['jshint', 'uglify']);
+
+};
---
+++ src/bootstrap-stripe.js
@@ -0,0 +1,39 @@
+// Define the jQuery Plugin
++function ($) {
+	'use strict';
+
+	// BOOSTRAP STRIPE CLASS DEFINITION
+	// ================================
+
+	var Stripe = function (element, options) {}
+
+	Stripe.VERSION = '0.0.0'
+
+	// BOOSTRAP STRIPE PLUGIN DEFINITION
+	// =================================
+
+	function Plugin(option) {
+		return this.each(function () {
+			var $this = $(this)
+			var data  = $this.data('bs.stripe')
+
+			if (!data) $this.data('bs.stripe', (data = new Stripe(this)))
+			if (typeof option == 'string') data[option].call($this)
+		})
+	} 
+
+	var old = $.fn.stripe
+
+	$.fn.stripe             = Plugin
+	$.fn.stripe.Constructor = Stripe
+
+
+	// BOOSTRAP STRIPE NO CONFLICT
+	// ===========================
+
+	$.fn.stripe.noConflict = function () {
+		$.fn.stripe = old
+		return this
+	}
+
+}(jQuery);
